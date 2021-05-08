013011fc1a4acad5c4b451572bcbfdb3257fc64b
Std1o <37378410+Std1o@users.noreply.github.com>
2021-05-08 17:37:08 +0300

    multiline text field SwiftUI

--- MultilineTextField/ContentView.swift
+++ MultilineTextField/ContentView.swift
@@ -1,15 +1,23 @@
-//
-//  ContentView.swift
-//  MultilineTextField
-//
-//  Created by Илья on 08.05.2021.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var text = ""
+    
+    // Auto Updating TextBox Height...
+    @State var containerHeight: CGFloat = 0
+    
     var body: some View {
-        Text("Hello, world!")
+        AutoSizingTF(hint: "Enter Message", text: $text,containerHeight: $containerHeight, onEnd: {
+            
+            // Do when keyboard closed...
+            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+        })
+            .padding(.horizontal)
+            // Your Max Height Here....
+            .frame(height: containerHeight <= 120 ? containerHeight : 120)
+        .background(Color.primary.opacity(0.04).ignoresSafeArea())
+            .cornerRadius(10)
             .padding()
     }
 }
