607a646169dc8c84fcb38353e41fce6724422da6
Std1o <37378410+Std1o@users.noreply.github.com>
2021-05-08 17:32:43 +0300

    Initial commit

---
+++ MultilineTextField/MultilineTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultilineTextFieldApp.swift
+//  MultilineTextField
+//
+//  Created by Илья on 08.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct MultilineTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
